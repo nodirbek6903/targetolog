d9f3914517302dba1280d9c09f96efc441985956
nodirchik6903 <nu2913634@gmail.com>
2024-05-10 23:29:08 +0500

    tarif responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
     <>
       {/* <Main /> */}
       {/* <About /> */}
-      <Service />
-      {/* <Tarif /> */}
+      {/* <Service /> */}
+      <Tarif />
       {/* <HomeCard /> */}
       {/* <Faq /> */}
     </>
